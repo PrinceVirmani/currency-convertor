130be3d5bed3e5266054d0942089cbdc9860f921
Prince <princevirmani410@gmail.com>
2024-05-19 18:14:59 +0530

    deploying it

---
+++ script.js
@@ -0,0 +1,61 @@
+const BASE_URL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromcurr = document.querySelector(".from select");
+const tocurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (e) => {
+    updateFlag(e.target);
+  });
+}
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  updateExchangeRate();
+});
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtval = amount.value;
+  if (amtval === "" || amtval < 1) {
+    amtval = 1;
+    amount.value = "1";
+  }
+
+  const URL = `${BASE_URL}/${fromcurr.value.toLowerCase()}/${tocurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let rate = data[tocurr.value.toLowerCase()];
+  console.log(rate);
+
+  let finalAmount = amtval * rate;
+  msg.innerText = `${amtval}${fromcurr.value} = ${finalAmount}${tocurr.value}`;
+};
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
